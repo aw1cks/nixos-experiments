05d06f6c929fe0811b93cb096dda35598ed04562
alex <alex@dziewanna.pub.awicks.io>
2025-08-20 01:52:05 +0100

    WIP: murmur TLS

--- machines/dziewanna/murmur.nix
+++ machines/dziewanna/murmur.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   services.murmur = {
     enable = true;
@@ -25,9 +25,25 @@
       messageburst=9999
       messagelimit=9999
     '';
+
+    sslKey = "${config.security.acme.certs."mumble.awicks.io".directory}/key.pem";
+    sslCert = "${config.security.acme.certs."mumble.awicks.io".directory}/fullchain.pem";
   };
 
-  # Allow murmur traffic
-  networking.firewall.allowedTCPPorts = [ 64738 ];
+  # Allow murmur traffic and HTTP for ACME challenge
+  networking.firewall.allowedTCPPorts = [ 64738 80 ];
   networking.firewall.allowedUDPPorts = [ 64738 ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "alex.wicks1@gmail.com";
+      listenHTTP = ":80";
+    };
+  };
+
+  security.acme.certs."mumble.awicks.io" = {
+    reloadServices = [ "murmur.service" ];
+    group = "murmur";
+  };
 }
